608b6cb353d430591e495869b2b838592eae7975
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-08-08 23:58:08 +0600

    Updated Code

--- pizza.js
+++ pizza.js
@@ -1,7 +1,14 @@
 const food = "Pizza"; // Replace with your desired food
 const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`;
 console.log(url); // Output: https://www.themealdb.com/api/json/v1/1/search.php?s=Pizza
+const disPlayPizza = (data) => {
+  console.log(data.meals[0]);
+};
 
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-.then(res => res.json())
-.then(data => console.log(data));
+const pizza = () => {
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+    .then(res => res.json())
+    .then(data => disPlayPizza(data));
+};
+
+pizza();
