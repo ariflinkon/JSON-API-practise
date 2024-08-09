2499a0a13fb8b0b4217533e7ec0db5e07043407e
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-08-09 23:39:40 +0600

    Added dynamic button

--- pizza.js
+++ pizza.js
@@ -1,14 +1,38 @@
-const food = "Pizza"; // Replace with your desired food
+/* const food = "Pizza"; // Replace with your desired food
 const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`;
-console.log(url); // Output: https://www.themealdb.com/api/json/v1/1/search.php?s=Pizza
+console.log(url); // Output: https://www.themealdb.com/api/json/v1/1/search.php?s=Pizza */
+
 const disPlayPizza = (data) => {
-  console.log(data.meals[0].idMeal);
+  const pizzaDiv = document.getElementById('pizzas');
+  const pizza = data.meals[0];
+
+  const pizzaCard = document.createElement('div');
+  pizzaCard.classList.add('pizza-card');
+
+  const pizzaImage = document.createElement('img');
+  pizzaImage.src = pizza.strMealThumb;
+  pizzaImage.alt = pizza.strMeal;
+  pizzaCard.appendChild(pizzaImage);
+ 
+  const pizzaId = document.createElement('h1');
+  pizzaId.textContent = pizza.idMeal;
+  pizzaCard.appendChild(pizzaId);
+
+  const pizzaTitle = document.createElement('h2');
+  pizzaTitle.textContent = pizza.strMeal;
+  pizzaCard.appendChild(pizzaTitle);
+
+  const pizzaInstructions = document.createElement('p');
+  pizzaInstructions.textContent = pizza.strInstructions;
+  pizzaCard.appendChild(pizzaInstructions);
+
+  pizzaDiv.appendChild(pizzaCard);
 };
 
 const pizza = () => {
-  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=pizza')
     .then(res => res.json())
     .then(data => disPlayPizza(data));
 };
-
 pizza();
+
