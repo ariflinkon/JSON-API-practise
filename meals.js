4dccfe278d3992ffbde0a86efb190a4a5dcdfdbf
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-08-17 23:43:48 +0600

    Using PUT to Update a User

--- meals.js
+++ meals.js
@@ -1,15 +1,26 @@
 const Meals = () => {
-  fetch('https://jsonplaceholder.typicode.com/comments')
+  const userData = {
+    id: 1,
+    name: 'Updated Name',
+    body: 'Updated body text'
+  };
+
+  fetch('https://jsonplaceholder.typicode.com/comments/1', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  })
     .then(res => res.json())
     .then(data => myDisplay(data));
-    
 }
 
 const myDisplay = (data) => {
   const newDiv = document.createElement('div');
   newDiv.id = 'newDiv';
-  newDiv.innerHTML = `<h2>${data[0].name}</h2>
-                      <p>${data[0].body}</p>`;
+  newDiv.innerHTML = `<h2>${data.name}</h2>
+                      <p>${data.body}</p>`;
   const container = document.getElementById('meals');
   container.appendChild(newDiv);
   console.log(data);
