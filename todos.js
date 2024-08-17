17982057b473973d17133e79539534c65de16862
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-08-17 23:58:19 +0600

    Using PATCH to Partially Update a User

--- todos.js
+++ todos.js
@@ -1,3 +1,16 @@
+const updateTodo = (id, updatedData) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedData)
+    })
+    .then(res => res.json())
+    .then(data => console.log('Todo updated:', data))
+    .catch(error => console.error('Error updating todo:', error));
+};
+
 const todos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
         .then(res => res.json())
@@ -6,7 +19,20 @@ const todos = () => {
 };
 
 const displayTodos = (data) => {
-    console.log(data);
+    const todosContainer = document.getElementById('todos-list');
+    data.forEach(todo => {
+        const todoItem = document.createElement('div');
+        todoItem.className = 'p-4 bg-gray-100 rounded shadow';
+        todoItem.textContent = todo.title;
+        todoItem.addEventListener('click', () => {
+            const updatedTitle = prompt('Enter new title:', todo.title);
+            if (updatedTitle) {
+                updateTodo(todo.id, { title: updatedTitle });
+                todoItem.textContent = updatedTitle;
+            }
+        });
+        todosContainer.appendChild(todoItem);
+    });
 };
 
 todos();
