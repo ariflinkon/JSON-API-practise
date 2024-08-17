99d9fe7a64372d6ef62942ba1f5c2ae1de6ab3d0
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-08-17 22:39:01 +0600

    Practise with DOM

--- meals.js
+++ meals.js
@@ -1,19 +1,18 @@
-const meals = () => {
-  fetch('http://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-  .then(res => res.json())
-  .then(data => {
-    const meal = data.meals[0];
-    const newDiv = document.createElement('div');
-    newDiv.id = 'newDiv';
-    newDiv.innerHTML = `<h2>${meal.strMeal}</h2>
-                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                        <p>${meal.strInstructions}</p>`;
-    container.appendChild(newDiv);
-  });
+const Meals = () => {
+  fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => myDisplay(data));
+    
 }
-meals();
 
-const container = document.getElementById('click');
-container.addEventListener('click', () => {
-  // No need to add anything here since the meals() function will handle the visual output
-});
+const myDisplay = (data) => {
+  const newDiv = document.createElement('div');
+  newDiv.id = 'newDiv';
+  newDiv.innerHTML = `<h2>${data[0].name}</h2>
+                      <p>${data[0].body}</p>`;
+  const container = document.getElementById('meals');
+  container.appendChild(newDiv);
+  console.log(data);
+}
+
+document.getElementById('submit').addEventListener('click', Meals);
