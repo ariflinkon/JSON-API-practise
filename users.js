d3ffe8d49f165b06f911602820de0a0209cb9f79
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-08-18 23:58:30 +0600

    Updated the users Json with DOM

--- users.js
+++ users.js
@@ -1,6 +1,14 @@
 const users = async () => {
-    const fetchRequest = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await fetchRequest.json();
-    console.log(data);
+    try {
+        const fetchRequest = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!fetchRequest.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await fetchRequest.json();
+        console.log(data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
 }
-users();
+
+document.getElementById('fetchUsers').addEventListener('click', users);
