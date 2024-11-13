83ea80a25765869b297aca8e48089cd6b756f5d5
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-11-13 23:55:41 +0600

    Updated meals file

--- meals.js
+++ meals.js
@@ -12,18 +12,28 @@ const Meals = () => {
     },
     body: JSON.stringify(userData)
   })
-    .then(res => res.json())
-    .then(data => myDisplay(data));
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return res.json();
+    })
+    .then(data => myDisplay(data))
+    .catch(error => console.error('There was a problem with the fetch operation:', error));
 }
 
 const myDisplay = (data) => {
-  const newDiv = document.createElement('div');
-  newDiv.id = 'newDiv';
-  newDiv.innerHTML = `<h2>${data.name}</h2>
-                      <p>${data.body}</p>`;
   const container = document.getElementById('meals');
-  container.appendChild(newDiv);
-  console.log(data);
+  if (container) {
+    const newDiv = document.createElement('div');
+    newDiv.id = 'newDiv';
+    newDiv.innerHTML = `<h2>${data.name}</h2>
+                        <p>${data.body}</p>`;
+    container.appendChild(newDiv);
+    console.log(data);
+  } else {
+    console.error('Container element not found');
+  }
 }
 
 document.getElementById('submit').addEventListener('click', Meals);
