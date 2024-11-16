d3d69b22bfccb6abd65cd27b8b0e81eea1699e00
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-11-16 23:46:17 +0600

    Updated the code

--- comments.js
+++ comments.js
@@ -1,6 +1,11 @@
-const comments = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/comments');
-    const data = await response.json();
-    console.log(data);
+async function fetchComments() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+    }
 }
-comments();
+
+fetchComments();
