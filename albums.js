aecf9bd195eb38a9535016a0dab32dc7d2bc28d7
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-11-24 23:58:39 +0600

    Updated code with another way

--- albums.js
+++ albums.js
@@ -1,8 +1,11 @@
-function fetchComments() {
-    fetch('https://jsonplaceholder.typicode.com/albums')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error fetching data:', error));
+async function fetchComments() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 fetchComments();
