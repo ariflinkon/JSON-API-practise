60d6d850b7d83849b4395d41feaa52d1391f347c
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-11-25 23:46:20 +0600

    Updated with another method

--- posts.js
+++ posts.js
@@ -1,7 +1,8 @@
-async function fetchPosts() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-    console.log(data);
+function fetchPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error fetching posts:', error));
 }
 
 fetchPosts();
