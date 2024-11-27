a7c4c8f2b3a78ccc84a279c95362aac1f01ea0d5
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-11-27 23:06:36 +0600

    Updated with another method

--- users.js
+++ users.js
@@ -1,14 +1,17 @@
-const fetchUsers = async () => {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const usersData = await response.json();
-        console.log(usersData);
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-    }
+function fetchUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(usersData => {
+            console.log(usersData);
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
 }
 
 document.getElementById('fetchUsers').addEventListener('click', fetchUsers);
