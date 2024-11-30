d48f6313ede6e83bed7ec2211ea1a4491298d864
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-12-01 00:00:27 +0600

    Used another method

--- posts.js
+++ posts.js
@@ -1,8 +1,14 @@
-function fetchPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error fetching posts:', error));
+async function fetchPosts() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
 }
 
 fetchPosts();
