04ce527053d476c3ab9978a687b65c071a064165
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-12-13 23:57:09 +0600

    Updated the code

--- meals.js
+++ meals.js
@@ -1,25 +1,28 @@
-const Meals = () => {
+const Meals = async () => {
   const userData = {
     id: 1,
     name: 'Updated Name',
     body: 'Updated body text'
   };
 
-  fetch('https://jsonplaceholder.typicode.com/comments/1', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userData)
-  })
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return res.json();
-    })
-    .then(data => myDisplay(data))
-    .catch(error => console.error('There was a problem with the fetch operation:', error));
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/comments/1', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    myDisplay(data);
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
 }
 
 const myDisplay = (data) => {
